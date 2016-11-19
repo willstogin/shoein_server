931ad7cf6a22a9d7828a46e450df0ea3992c3107
William <williamstogin2018@u.northwestern.edu>
2016-11-19 17:36:35 -0600

    Cleanup with john

--- arduinocom/bitsend/bitsend.ino
+++ arduinocom/bitsend/bitsend.ino
@@ -15,21 +15,28 @@ void setup() {
 
 // Reads the pin and writes the bit to the rightmost bit of char, returning result
 byte getBit(byte b) {
+  while(digitalRead(inClock) == LOW);
   while(digitalRead(inClock) == HIGH);
   return (b << 1) | ((digitalRead(inPin) == HIGH) ? 1 : 0);  
 }
 
 // Consumes the leftmost bit and returns the resulting char
 byte sendBit(byte b) {
+  int delay = 1000; // 100 corresponds to 9600 baud
   digitalWrite(outClock, HIGH);
   digitalWrite(outPin, ((b & 0x80) ? HIGH : LOW));
-  delayMicroseconds(1000000); // Corresponds to 9600 baud
+  delayMicroseconds(delay/2); 
   digitalWrite(outClock, LOW);
-  Serial.print("Done");
-  Serial.print(b);
-  Serial.print("\n");
-  
+  delayMicroseconds(delay/2);
   return b << 1;
+}
+
+byte getByte() {
+  byte accumulator = '\0';
+  for (int i=0; i<8; i++) {
+    accumulator = getBit(accumulator);
+  }
+  return accumulator;
 }
 
 void sendByte(byte b) {
@@ -37,34 +44,15 @@ void sendByte(byte b) {
   for (int i=0; i<8; i++) {
     toSend = sendBit(toSend);
   }
-  byte res = '\0';
-  res = getBit(res);
-  if (!res) {
-    sendByte(b);
-  }
 }
 
-int x = 0;
+byte x = 1;
 char writer = 1;
 void loop() {
   if (writer) {
-    
-    byte a = (byte) x;
-    int c = 0;
-    c += (int)a;
-    for (int i=0; i<8; i++) {
-      a = sendBit(a);
-    }
-//    Serial.print("Done");
-//    Serial.print(c);
- //   Serial.print("\n");
-    x++;
+    sendByte(x);
   } else {
-    byte b = '\0';
-    for (int i=0; i<8; i++) {
-      b = getBit(b);
-    }
-    Serial.print("B is ");
+    byte b = getByte();
     Serial.print(b);
     Serial.print("\n");
   }

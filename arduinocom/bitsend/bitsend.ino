c94c13f0bbc685eccf195b9a6e74f3ea70c23850
Jerry Sun <Jerry Sun>
2016-11-19 17:07:15 -0600

    took williams version

--- arduinocom/bitsend/bitsend.ino
+++ arduinocom/bitsend/bitsend.ino
@@ -6,6 +6,7 @@ int outPin = 11;
 
 void setup() {
   // put your setup code here, to run once:
+  //i made a change
   Serial.begin(9600);
   pinMode(outPin, OUTPUT);
   pinMode(inPin, INPUT);
@@ -13,10 +14,19 @@ void setup() {
   pinMode(outClock, OUTPUT);
 }
 
+<<<<<<< HEAD
+// Reads the pin and writes the bit to the rightmost bit of byte1, returning result
+char getBit(char byte1) {
+  while(digitalRead(inClock) == LOW);
+  while(digitalRead(inClock) == HIGH);
+
+  return (byte << 1) | ((digitalRead(inPin) == HIGH) ? 1 : 0);  
+=======
 // Reads the pin and writes the bit to the rightmost bit of char, returning result
 byte getBit(byte b) {
   while(digitalRead(inClock) == HIGH);
   return (b << 1) | ((digitalRead(inPin) == HIGH) ? 1 : 0);  
+>>>>>>> 9581da7feb9d00c483cfcf6a188184ae75faa3eb
 }
 
 // Consumes the leftmost bit and returns the resulting char
@@ -45,9 +55,9 @@ void sendByte(byte b) {
 }
 
 int x = 0;
-char writer = 1;
+char writer = 0;
 void loop() {
-  if (writer) {
+  if (writer) {//writing
     
     byte a = (byte) x;
     int c = 0;
@@ -59,13 +69,21 @@ void loop() {
 //    Serial.print(c);
  //   Serial.print("\n");
     x++;
+<<<<<<< HEAD
+  } else {//reading
+    char b = '\0';
+=======
   } else {
     byte b = '\0';
+>>>>>>> 9581da7feb9d00c483cfcf6a188184ae75faa3eb
     for (int i=0; i<8; i++) {
       b = getBit(b);
+      Serial.print(b);
     }
-    Serial.print("B is ");
-    Serial.print(b);
+    int c = 0;
+    c = c + (int) b;
+    Serial.print("C is ");
+    Serial.print(c);
     Serial.print("\n");
   }
 }
